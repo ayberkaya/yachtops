9aca8346477ebec36749026795180319769e0393
Ayberk Kaya <kaya@192.168.1.11>
2025-12-04 21:12:51 +0300

    Add basePath and refetch options to SessionProvider to fix client-side exceptions

--- components/providers/session-provider.tsx
+++ components/providers/session-provider.tsx
@@ -4,7 +4,11 @@ import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <SessionProvider
+      basePath="/api/auth"
+      refetchInterval={0}
+      refetchOnWindowFocus={false}
+    >
       {children}
     </SessionProvider>
   );
