3ef8a04a1652409bea9456367006a3902e8b29a1
Ayberk Kaya <kaya@Ayberks-MacBook-Pro.local>
2025-12-06 16:04:43 +0300

    fix: define collapsible components to avoid runtime error

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,6 +1,23 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 
-export const Collapsible = CollapsiblePrimitive.Root;
-export const CollapsibleTrigger = CollapsiblePrimitive.Trigger;
-export const CollapsibleContent = CollapsiblePrimitive.Content;
+const Collapsible = CollapsiblePrimitive.Root;
+
+const CollapsibleTrigger = forwardRef<
+  ElementRef<typeof CollapsiblePrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Trigger ref={ref} className={className} {...props} />
+));
+CollapsibleTrigger.displayName = CollapsiblePrimitive.Trigger.displayName;
+
+const CollapsibleContent = forwardRef<
+  ElementRef<typeof CollapsiblePrimitive.Content>,
+  ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Content ref={ref} className={className} {...props} />
+));
+CollapsibleContent.displayName = CollapsiblePrimitive.Content.displayName;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
 
